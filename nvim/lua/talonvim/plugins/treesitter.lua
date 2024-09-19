f8dbc5364a1a9bb9e1976b39742b0092770e54f8
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-09-18 22:41:23 -0400

    add yaml treesitter

--- nvim/lua/talonvim/plugins/treesitter.lua
+++ nvim/lua/talonvim/plugins/treesitter.lua
@@ -15,6 +15,7 @@ return { -- Highlight, edit, and navigate code
       'vim',
       'vimdoc',
       'norg',
+      'yaml',
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
