9c41f23da98ae99712abb71b7656057a17d2d8f8
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-09-19 22:42:24 -0400

    optimze aerospace workspaces and enable transparency

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,17 +11,18 @@ config.color_scheme = 'Tokyo Night'
 config.window_decorations = 'RESIZE'
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font_with_fallback {
-  { family = 'JetBrainsMono Nerd Font', scale = 1.4 },
+  { family = 'JetBrainsMono Nerd Font', scale = 1.3 },
 }
 config.window_close_confirmation = 'NeverPrompt'
 config.scrollback_lines = 3000
 config.use_fancy_tab_bar = false
+config.window_background_opacity = 0.95
 config.audible_bell = 'Disabled'
 config.window_padding = {
-  left = 25,
-  right = 25,
-  top = 25,
-  bottom = 25,
+  left = 15,
+  right = 15,
+  top = 15,
+  bottom = 15,
 }
 
 -- Keys
