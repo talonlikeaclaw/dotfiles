589ebd80d02472115776ad0209230c0c6043bf4f
talonlikeaclaw <talonlikeaclaw@proton.me>
2024-09-23 20:08:15 -0400

    add jj escape keymap and add maven supoort

--- nvim/lua/talonvim/plugins/betterescape.lua
+++ nvim/lua/talonvim/plugins/betterescape.lua
@@ -1,6 +1,37 @@
 return {
   'max397574/better-escape.nvim',
   config = function()
-    require('better_escape').setup()
+    require('better_escape').setup {
+      mappings = {
+        i = {
+          j = {
+            -- These can all also be functions
+            k = false,
+            j = '<Esc>',
+          },
+        },
+        c = {
+          j = {
+            k = false,
+            j = '<Esc>',
+          },
+        },
+        t = {
+          j = {
+            k = '<C-\\><C-n>',
+          },
+        },
+        v = {
+          j = {
+            k = false,
+          },
+        },
+        s = {
+          j = {
+            k = false,
+          },
+        },
+      },
+    }
   end,
 }
