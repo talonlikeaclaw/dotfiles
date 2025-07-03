d96e66f357423fe576c13248babecfa7b1d9b075
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-07-03 13:55:19 -0400

    remove configurations for applications not in use on linux

--- nvim/lua/talonvim/core/init.lua
+++
@@ -1,3 +0,0 @@
-require 'talonvim.core.keymaps'
-require 'talonvim.core.lazy'
-require 'talonvim.core.options'
--- nvim/lua/talonvim/plugins/mini.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'echasnovski/mini.nvim',
-  config = function()
-    require('mini.ai').setup { n_lines = 500 }
-    require('mini.sessions').setup()
-    require('mini.surround').setup()
-    require('mini.icons').setup()
-  end,
-}
