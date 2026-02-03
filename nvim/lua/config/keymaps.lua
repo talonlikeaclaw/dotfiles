dfb9edbb289713b802f965c19ab54a3ed12b3e47
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-02-02 20:29:08 -0500

    Configure LazyVim for useability

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Exit nromal mode via "jj"
-vim.keymap.set("i", "jj", "<Esc>", { silent = true })
+vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save File" })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit Insert Mode" })
 
--- Disable Ctrl + s to save and switch to "<leader>fs"
-vim.keymap.del("n", "<C-s>")
-vim.keymap.set("n", "<leader>fs", ":w<CR>", { desc = "Save file" })
+-- Disable default C-s save (LazyVim sets it in n, i, x, s modes)
+vim.keymap.del({ "n", "i", "x", "s" }, "<C-s>")
---
+++ nvim/lua/plugins/extend-blink.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<CR>"] = {},
+        ["<C-j>"] = { "accept", "fallback" },
+      },
+    },
+  },
+}
