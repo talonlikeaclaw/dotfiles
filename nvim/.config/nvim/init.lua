7fc258f17307c9efc8aabc9071f7ddc9d8c054a4
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-04-06 21:56:44 -0400

    chore: replace old LazyVim nvim config with current nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,15 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- Neovim v0.12.0 Configuration
+-- Resources:
+-- 	- https://echasnovski.com/blog/2026-03-13-a-guide-to-vim-pack
+-- 	- https://neovim.io/doc/user/lsp/
+
+vim.loader.enable()
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require('builtins')
+require('options')
+require('keymaps')
+require('plugins')
+require('lsp')
