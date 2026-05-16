df3fabed5752d31c12e2066480b3b1c83a341a4c
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-05-16 18:00:35 -0400

    Add git/.gitconfig configuration and add to install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 OS=$(uname)
 
-SHARED=(bat fastfetch ghostty gitmux helix kitty nvim opencode starship television tmux wezterm)
+SHARED=(bat fastfetch git ghostty gitmux helix kitty nvim opencode starship television tmux wezterm)
 
 if [[ "$OS" == "Darwin" ]]; then
     PLATFORM=(aerospace llama-swap-mac zed-mac zsh-mac)
